4102a8c944bd7c6421857adf7e71343ba48d695c
Joshua Kang <joshuakang@Joshuas-MacBook-Pro-7.local>
2019-04-25 17:35:03 -0700

    added the part 2 and outoput the training validate and test error

--- pa1.py
+++ pa1.py
@@ -1,64 +1,76 @@
 import math
-import numpy as np
 from collections import Counter
+import copy
+import numpy as np
 
 
-
-def loadTrainingData(trainingFile, testingFile, validationFile, trainingSet=[], testSet=[], validationSet=[]): 
-    trainingSetFile = open (trainingFile, "r")
+def loadTrainingData(trainingFile, testingFile, validationFile, projectionFile, trainingSet=[], testSet=[], validationSet=[], projectionSet=[], trainingSetLabel=[], testSetLabel=[], validationSetLabel=[]):
+    trainingSetFile = open(trainingFile, "r")
     traininglines = trainingSetFile.readlines()
     trainingSetFile.close()
-    for unFormattedLine in traininglines: 
+    for unFormattedLine in traininglines:
         line = unFormattedLine.split()
         currentLine = []
-        for char in line:
+        for char in line[:-1]:
             currentLine.append(int(char))
         trainingSet.append(currentLine)
+        trainingSetLabel.append(line[len(line)-1])
 
-    testingSetFile = open (testingFile, "r")
+    projectionSetFile = open(projectionFile, "r")
+    projectionlines = projectionSetFile.readlines()
+    projectionSetFile.close()
+    for unFormattedLine in projectionlines:
+        line = unFormattedLine.split()
+        currentLine = []
+        for char in line:
+            currentLine.append(float(char))
+        projectionSet.append(currentLine)
+
+    testingSetFile = open(testingFile, "r")
     testingLines = testingSetFile.readlines()
     testingSetFile.close()
-    for unFormattedLine in testingLines: 
+    for unFormattedLine in testingLines:
         line = unFormattedLine.split()
         currentLine = []
-        for char in line:
-            currentLine.append(int(char))        
+        for char in line[:-1]:
+            currentLine.append(int(char))
         testSet.append(currentLine)
+        testSetLabel.append(line[len(line)-1])
 
-    validationSetFile = open (validationFile, "r")
+    validationSetFile = open(validationFile, "r")
     validationLines = validationSetFile.readlines()
     validationSetFile.close()
-    for unFormattedLine in validationLines: 
+    for unFormattedLine in validationLines:
         line = unFormattedLine.split()
         currentLine = []
-        for char in line:
+        for char in line[:-1]:
             currentLine.append(int(char))
         validationSet.append(currentLine)
+        validationSetLabel.append(line[len(line)-1])
 
-#returns the accuracy
-def knn(k, testPoints, trainingSet):
+
+def runKnn(k, testSet, trainingSet, testSetLabel, trainingSetLabel):
+    predictions = knn(k, testSet, trainingSet, trainingSetLabel)
+    accuracy = getAccuracy(predictions, testSetLabel)
+    print(str(k) + ' : ' + str(accuracy))
+
+    return accuracy
+
+
+def knn(k, testPoints, trainingSet, trainingSetLabel):
 
     predictions = []
     for point in testPoints:
-        prediction = predict(k, point, trainingSet)
+        prediction = predict(k, point, trainingSet, trainingSetLabel)
         predictions.append(prediction)
-    
+
     return predictions
 
-def getAccuracy(predictions, testPoints):
-    i = 0
-    numCorrect = 0
-    for prediction in predictions:
-        print(str(prediction) + ' : ' + str(testPoints[i][-1]))
-        if int(prediction) == int(testPoints[i][-1]):
-            numCorrect += 1
-            print('yer mams a  hoe' + str(numCorrect))
 
-        i += 1
-    return numCorrect/len(testPoints)
+# outputs the most common of the neighbors in the neigborhood of k
 
-#outputs the most common of the neighbors in the neigborhood of k 
-def predict(k, test, trainingSet): 
+
+def predict(k, test, trainingSet, trainingSetLabel):
     distances = []
     neighborhood = []
 
@@ -66,51 +78,114 @@ def predict(k, test, trainingSet):
     for trainingPoint in trainingSet:
         distances.append([getDistance(trainingPoint, test), i])
         i += 1
-        
-    
+
     distances = sorted(distances)
 
     for i in range(k):
-        neighborhood.append(trainingSet[distances[i][1]][-1])
+        neighborhood.append(trainingSetLabel[distances[i][1]])
 
     data = Counter(neighborhood)
 
-    print(str(neighborhood) + ' : ' + str(max(neighborhood, key=data.get)))
+    #print(str(neighborhood) + ' : ' + str(max(neighborhood, key=data.get)))
+
     return max(neighborhood, key=data.get)
 
 
-
-
-
-def getDistance(first, second): 
-    firstList = np.array(first[:-1])
-    secondList =  np.array(second[:-1])
+def getDistance(first, second):
+    firstList = np.array(first)
+    secondList = np.array(second)
 
     dist = np.linalg.norm(firstList-secondList)
 
     return dist
 
-def testLoadTrainingData(filename, trainingSet=[], testSet=[], validationSet=[]):
-    trainingSetFile = open (filename, "r")
-    trainingline = trainingSetFile.readline()
-    trainingSetFile.close()
-    current = []
-    for char in trainingline:
-        current.append(char)
+# returns the accuracy
+def getAccuracy(predictions, testSetLabel):
+    i = 0
+    numCorrect = 0
+    for prediction in predictions:
+        if int(prediction) == int(testSetLabel[i]):
+            #print("ur mam's a hoe " + str(numCorrect))
+            numCorrect += 1
+        i += 1
+    return (1 - float(numCorrect)/float(len(testSetLabel)))
 
 
-    
+def project(subject, target):
+    return np.matmul(subject, target)
+
+
 trainingSet = []
 testSet = []
 validationSet = []
+trainingSetLabel = []
 
-#testLoadTrainingData('pa1train.txt', trainingSet, testSet, validationSet)
+testSetLabel = []
+validationSetLabel = []
+projectionSet = []
 
-loadTrainingData('pa1train.txt', 'pa1train.txt', 'pa1validate.txt', trainingSet, testSet , validationSet)
-predictions = knn(3, testSet, trainingSet)
-accuracy = getAccuracy(predictions, testSet)
-print(accuracy) 
+newTrainingSet = []
+newTestSet = []
+newValidationSet = []
 
 
-x = predict(3, testSet[700], trainingSet)
-print(x)
+loadTrainingData('pa1train.txt', 'pa1train.txt', 'pa1validate.txt',
+                 'projection.txt', trainingSet, testSet, validationSet, projectionSet, trainingSetLabel, testSetLabel, validationSetLabel)
+
+print("Computing training error on the original dataset")
+oneTrainingOriginal = runKnn(1, trainingSet, trainingSet, trainingSetLabel, trainingSetLabel)
+threeTrainingOrignal = runKnn(3, trainingSet, trainingSet, trainingSetLabel, trainingSetLabel)
+fiveTrainingOringal = runKnn(5, trainingSet, trainingSet, trainingSetLabel, trainingSetLabel)
+nineTrainingOriginal = runKnn(9, trainingSet, trainingSet, trainingSetLabel, trainingSetLabel)
+fifteenTrainingOriginal = runKnn(15, trainingSet, trainingSet, trainingSetLabel, trainingSetLabel)
+
+print("computing the validation error on the orignal dataset")
+validationOriginal = []
+oneValidationOriginal = runKnn(1, validationSet, trainingSet, validationSetLabel, trainingSetLabel)
+threeValidationOriginal = runKnn(3, validationSet, trainingSet, validationSetLabel, trainingSetLabel)
+fiveValidationOriginal = runKnn(5, validationSet, trainingSet, validationSetLabel, trainingSetLabel)
+nineValidationOriginal = runKnn(9, validationSet, trainingSet, validationSetLabel, trainingSetLabel)
+fifteenValidationOriginal = runKnn(15, validationSet, trainingSet, validationSetLabel, trainingSetLabel)
+
+validationOriginal.append([oneValidationOriginal, 1])
+validationOriginal.append([fiveValidationOriginal, 5])
+validationOriginal.append([nineValidationOriginal, 9]) 
+validationOriginal.append([fifteenValidationOriginal, 15])
+originalMin = min(validationOriginal)
+
+testErrorOriginal = runKnn(originalMin[1], testSet, trainingSet, testSetLabel, trainingSetLabel)
+
+#Part 2
+print("creating the new projected data")
+tcopy = copy.deepcopy(trainingSet)
+newTrainingSet = project(tcopy, projectionSet)
+
+zcopy = copy.deepcopy(testSet)
+newTestSet = project(zcopy, projectionSet)
+
+ccopy = copy.deepcopy(validationSet)
+newValidationSet = project(ccopy, projectionSet)
+
+print("Computing the training error on the projected dataset")
+oneTrainingProjected = runKnn(1, newTrainingSet, newTrainingSet, trainingSetLabel, trainingSetLabel)
+threeTrainingProjected = runKnn(3, newTrainingSet, newTrainingSet, trainingSetLabel, trainingSetLabel)
+fiveTrainingProjected = runKnn(5, newTrainingSet, newTrainingSet, trainingSetLabel, trainingSetLabel)
+nineTrainingProjected = runKnn(9, newTrainingSet, newTrainingSet, trainingSetLabel, trainingSetLabel)
+fifteenTrainingProjected = runKnn(15, newTrainingSet, newTrainingSet, trainingSetLabel, trainingSetLabel)
+
+print("Computing the validaiton error on the projected dataset")
+validationProjected = []
+oneValidationProjected = runKnn(1, newValidationSet, newTrainingSet, trainingSetLabel, trainingSetLabel)
+threeValidationProjected = runKnn(3, newValidationSet, newTrainingSet, trainingSetLabel, trainingSetLabel)
+fiveValidationProjected = runKnn(5, newValidationSet, newTrainingSet, trainingSetLabel, trainingSetLabel)
+nineValidationProjected = runKnn(9, newValidationSet, newTrainingSet, trainingSetLabel, trainingSetLabel)
+fifteenValidationProjected = runKnn(15, newValidationSet, newTrainingSet, trainingSetLabel, trainingSetLabel)
+
+validationProjected.append([oneValidationProjected, 1])
+validationProjected.append([fiveValidationProjected, 5])
+validationProjected.append([nineValidationProjected, 9])
+validationProjected.append([fifteenValidationProjected, 15])
+
+projectedMin = min(validationProjected)
+
+testErrorProjected = runKnn(projectedMin[1], newTestSet, trainingSet, testSetLabel, trainingSetLabel)
